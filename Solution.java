dc3ec3ece981dd0dddeefc9ad1b020df1f8136a3
sdim-ua <ingersol@yandex.ua>
2019-01-30 21:04:26 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,10 @@
+/* 
+Вывести на экран сегодняшнюю дату
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        System.out.println("30" +  " 01" + " 2019");
+    }
+}
